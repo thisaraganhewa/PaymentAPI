84ed1b35d70cb29f12e7bb13e1f306edc1345df7
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-12-08 17:58:05 +0530

    added constructor for the payment details context

--- PaymentAPI/PaymentAPI/Models/PaymentDetailsContext.cs
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailsContext.cs
@@ -4,5 +4,11 @@ namespace PaymentAPI.Models
 {
     public class PaymentDetailsContext:DbContext
     {
+
+        public PaymentDetailsContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
     }
 }
