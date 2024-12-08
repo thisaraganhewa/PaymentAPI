f5bbe96a6279d16310d3f86c1f013df969e7bdf9
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-12-08 17:42:07 +0530

    created a model called Payment deatsil and addded details

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PaymentId { get; set; }
+        public string CardOwnerNmae { get; set; } = "";
+        public string CardNumber { get; set; } = "";
+        public string ExpirationDate { get; set; } = "";
+        public string SecurityCode { get; set; } = "";
+
+    }
+}
