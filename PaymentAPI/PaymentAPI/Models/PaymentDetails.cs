9989eda77eb1ae742972538498344fc185eaba5c
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-12-08 17:48:06 +0530

    added column types to each column in PaymentDetails.cs

--- PaymentAPI/PaymentAPI/Models/PaymentDetails.cs
+++ PaymentAPI/PaymentAPI/Models/PaymentDetails.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PaymentAPI.Models
 {
@@ -6,9 +7,17 @@ namespace PaymentAPI.Models
     {
         [Key]
         public int PaymentId { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
         public string CardOwnerNmae { get; set; } = "";
+
+        [Column(TypeName ="nvarchar(16)")]
         public string CardNumber { get; set; } = "";
+
+        [Column(TypeName ="nvarchar(5)")]
         public string ExpirationDate { get; set; } = "";
+
+        [Column(TypeName ="nvarchar(3)")]
         public string SecurityCode { get; set; } = "";
 
     }
